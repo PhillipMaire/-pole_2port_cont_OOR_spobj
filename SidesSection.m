47d9bc73e14fc7275a510f476c917f55805f59ec
jonathansy <jonathbs@usc.edu>
2016-05-26 16:31:51 -0700

    testing git hub

--- SidesSection.m
+++ SidesSection.m
@@ -1,5 +1,5 @@
 % [x, y] = SidesSection(obj, action, x, y)
-%testing git hub 
+%
 % Section that takes care of choosing the next correct side and keeping
 % track of a plot of sides and hit/miss history.
 %
